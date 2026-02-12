7b2187130afe00c98b2ed7ba09da08199fb7e220
IrafashCactus <141725199+IrafashCactus@users.noreply.github.com>
2026-02-12 13:46:31 +0200

    Add files via upload
    extented version

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // <--- THIS MUST BE HERE
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
